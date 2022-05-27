83072771901c4968df6efc4cb9f532060b48a547
Supun <nissankasupunrasanga@gmail.com>
2022-05-27 20:38:08 +0530

    Signing Form Group Added

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  signingForm !: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.signingForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
   }
-
 }
